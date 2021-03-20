dfb4142484289ca71f35f16db8c39695757f67c1
Eduardo <eduardo.bm10@estudiantec.cr>
2021-03-19 19:35:36 -0600

    Crear clase pruebathrow

--- src/ManejoExcepciones.java
+++ src/ManejoExcepciones.java
@@ -1,7 +1,15 @@
-//Eduardo Bolívar: Tarea Extraclase 1.
-//Manejo de excepciones en Java.
+//Tarea Extraclase - Algoritmos y Estructuras de Datos I
+//Nombre: Eduardo Bolivar Minguet
+//Carne: 2020158103
 
+//Clase con metodo main para invocar a cada clase ejemplo
 public class ManejoExcepciones
 {
+    public static void main(String[] args)
+    {
+        PruebaTryCatch tryCatch = new PruebaTryCatch();
 
+        System.out.print(tryCatch.read());
+
+    }
 }
---
+++ src/PruebaTryCatch.java
@@ -0,0 +1,11 @@
+//Tarea Extraclase - Algoritmos y Estructuras de Datos I
+//Nombre: Eduardo Bolivar Minguet
+//Carne: 2020158103
+
+public class PruebaTryCatch
+{
+    public void read()
+    {
+
+    }
+}
